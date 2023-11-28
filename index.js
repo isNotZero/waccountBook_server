5b40e87bd84e08bb2d82525fb9be391e883d753f
isnotzero89 <isnotzero89@gmail.com>
2023-11-28 15:02:51 +0900

    cors 추가

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 import dataRouter from './router/data.js';
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/data', dataRouter);
 
